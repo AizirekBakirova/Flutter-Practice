26424e6683939ea69f3f6e527ce31de059916f20
Aizirek Bakirova <aizirek.bakirova@gmail.com>
2023-10-03 23:31:53 +0600

    on process

---
+++ flutter_test_application/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test_application/pages/login_page.dart';
+import 'package:flutter_test_application/styles/app_colors.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+            fontFamily: "Urbanist",
+            scaffoldBackgroundColor: AppColors.background),
+        home: LoginPage());
+  }
+}
